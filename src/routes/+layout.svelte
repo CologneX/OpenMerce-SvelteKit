ed6f2448ee354cc1938f0fc2ad7653dc5a5c403e
Kyrell Asus <kleanosiauw@student.ciputra.ac.id>
2023-04-03 20:48:34 +0700

    #1 first commit, header still on progress

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import Logo from '$lib/logo.svelte';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<span><Logo /></span>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<span>WEB DEV</span>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
