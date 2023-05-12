c4af3eb0db127786940e430166fb5dcabf76e69e
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2023-05-12 10:14:49 +0700

    feat(devops): add Dockerfile and change adapter into node

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapterNode from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,11 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// adapter-node as we are using docker to run the app
 		adapter: adapter(),
-		
 	}
 };
 
