f059a19f78d4a7e2d68b7b2e01b57407ddf96f09
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-06-06 15:14:39 +0700

    feat(transaction-list): added transaction list route

---
+++ src/routes/(user)/transaction-list/+page.svelte
@@ -0,0 +1,7 @@
+<script lang>
+	import { goto } from '$app/navigation';
+</script>
+
+<button class="btn btn-xl" on:click={() => goto('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}
+	>KLICK DISINI UNTUNG YANG ENA-ENA</button
+>
